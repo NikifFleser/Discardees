c90576f60161f71431a2b4676798325d631e2cd8
Markus Fiedler <palaeontologix@web.de>
2025-09-05 21:56:21 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Discardees
+
+## Summary
+*Cemal: ToDo*
+
+## Setup
+Create virtual environment:
+```console
+python -m venv venv
+```
+Activate virtual environment:
+```console
+./venv/Scripts/activate
+```
---
+++ src/main.py
@@ -0,0 +1,9 @@
+def main():
+    """
+    Main entry point for the application.
+    """
+    print("Welcome to the application!")
+    # TODO: Implement further functionality
+
+if __name__ == "__main__":
+    main()
