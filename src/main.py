4b8afe1fb955a92c6f5169c5e8cd3f9c5cb8f924
pengilton <94359705+pengilton@users.noreply.github.com>
2025-09-05 23:13:46 +0200

    Initial commit

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ def main():
     Main entry point for the application.
     """
     print("Welcome to the application!")
+    print("Hello World!")
     # TODO: Implement further functionality
 
 if __name__ == "__main__":
